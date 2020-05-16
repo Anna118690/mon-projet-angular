0f690855d3ffc7cde7503a08bbcfbb40cb149ba2
AnnaLaptop <anna.laskowska@hotmail.com>
2020-05-16 22:15:28 +0200

    services onSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,45 +8,21 @@ import { AppareilService} from './services/appareil.service';
 })
 export class AppComponent {
 
-  isAuth = false;
-  
-
-  appareils: any[]; //un array de type any
+ 
 
 
 
 
- constructor(private appareilService: AppareilService) {
-   setTimeout(
-     () =>{
-       this.isAuth = true;
-     },  2000
-   );
- }
+
+
+
 
  //pour que AppComponent puisse recupere les informatopns stockess ands AppareilSercice nous devons implementer la methode ngOnInit()
  //umieszczamy po konstruktorze ale przed innymi metodami komponentu
  //teraz mozemy odzyskac informacje z AppareilService w metodzie ngOnInit()
 
- ngOnInit() {
-  this.appareils = this.appareilService.appareils;
-}
+ 
 
- onAllumer() {
-  if(confirm('Etes-vous sûr de vouloir allumer tous vos appareils ?')) {
-    this.appareilService.switchOnAll();
-  } else {
-    return null;
-  }
-  
- }
-
- onEteindre() {
-  if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-    this.appareilService.switchOffAll();
-  } else {
-    return null;
-  }
-}
+ 
 
 }
